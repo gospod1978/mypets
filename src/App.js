c4d904ddab2e211945c2df3e23d767649b0c622d
Mihail Gospodinov <54326812+gospod1978@users.noreply.github.com>
2021-05-03 17:33:50 +0200

    some error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Categories from './components/Categories/Categories'
 import PetDetails from './components/PetDetails/PetDetails'
-import DemoPage from './components/Demo/DemoPage'
-import DemoPage2 from './components/Demo/DemoPage2'
-
 import './App.css';
 
 function App() {
@@ -18,8 +15,6 @@ function App() {
         <Route path='/' exact component={Categories} />
         <Route path='/categories/:category' exact component={Categories} />
         <Route path='/pets/details/:petId' exact component={PetDetails} />
-        <Route path='/demo' exact component={DemoPage} />
-        <Route path='/demo2' exact component={DemoPage2} />
       </Switch>
 
       <Footer />
@@ -30,4 +25,4 @@ function App() {
 export default App;
 
 
-// 9 lekcia/ 1:49
+// 2:07
