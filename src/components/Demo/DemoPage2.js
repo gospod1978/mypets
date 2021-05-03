7388f78040cffed3f573c46952dec9f56830f87a
Mihail Gospodinov <54326812+gospod1978@users.noreply.github.com>
2021-05-03 17:33:11 +0200

    update

--- src/components/Demo/DemoPage2.js
+++ src/components/Demo/DemoPage2.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 
 const options = [
     {label:'IT', value:'it'},
@@ -6,25 +6,31 @@ const options = [
     {label:'CV', value:'cv'},
     {label:'RM', value:'rm'},
 ]
-export default class DemoPage1 extends Component {
+class DemoPage2 extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: '',
+            username: 'Pesho',
             age: 18,
             bio: 'Lorem ipsum',
-            occupation: 'rm'
+            occupation: 'rm',
         }
         this.onChangeHandler = this.onChangeHandler.bind(this)
+        // this.emailInput = React.createRef()
+    }
+    componentDidMount() {
+        console.log(this.state)
     }
     onSubmitHandler(e)  {
         e.preventDefault()
-        console.log(e.target.username.value)
-        console.log(e.target.age.value)
+        // const {username, age} = this.state;
+        console.log(this.state)
+        console.log(this.emailInput.current)
     }
     //generic onChange method
     onChangeHandler(e) {
         this.setState({[e.target.name]: e.target.value})
+        console.log(e.target.username)
     }
     render() {
         return (
@@ -35,6 +41,10 @@ export default class DemoPage1 extends Component {
                     <form onSubmit={this.onSubmitHandler}>
                         <label htmlFor="username">Username</label>
                         <input type="text" id="username" placeholder="Pesho" name="username" value={this.state.username} onChange={this.onChangeHandler} />
+                        <br />
+                        {/* <label htmlFor="email">Email</label> */}
+                        {/* <input ref={this.emailInput} type="email" id="email" name="email" placeholder="example@gmsil.com"/> */}
+                        <br />
                         <label htmlFor="age">Age</label>
                         <input type="number" id="age" value={this.state.age} name="age" onChange={this.onChangeHandler}/>
                         <br />
@@ -56,3 +66,5 @@ export default class DemoPage1 extends Component {
         )
     }
 }
+
+export default DemoPage2
